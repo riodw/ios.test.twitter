c0ea69e5c19599d65fc7ecd812bd683b9ba32187
riow <rio@medtricslab.com>
2017-07-07 21:11:11 -0400

    [master] All Files; Initial commit;

---
+++ HomeDatasourceController.swift
@@ -0,0 +1,32 @@
+//
+//  HomeDatasourceController.swift
+//  Project-1
+//
+//  Created by Riorden Weber on 7/6/17.
+//  Copyright © 2017 Riorden Weber. All rights reserved.
+//
+
+import LBTAComponents
+
+class HomeDatasourceController: DatasourceController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // collectionView?.backgroundColor = .red
+        
+        let homeDatasource = HomeDatasource()
+        self.datasource = homeDatasource
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 150)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width, height: 50)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width, height: 50)
+    }
+    
+}
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Project-1' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Project-1
+  pod 'LBTAComponents', '~> 0.1.9'
+
+end
